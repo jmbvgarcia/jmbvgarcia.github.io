8017ade6380b6144c958d68b6d20e1ab318002b1
jmbvgarcia <55067473+jmbvgarcia@users.noreply.github.com>
2025-04-16 00:52:17 +0000

    Deploying to gh-pages from @ jmbvgarcia/jmbvgarcia.github.io@acf314c38ec78324d94f721bb6fe868d2bde8d51 🚀

---
+++ assets/code/imperfect_compliance.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+library(ivreg)
+set.seed(12345)
+
+# Number of observations
+n <- 10000
+
+# Compliance types
+types <- sample(c("complier", "always", "never"),
+                n, replace = TRUE, prob = c(0.5, 0.25, 0.25))
+
+# Generate tibble with baseline potential outcome Y0 depending on type
+df <- tibble(
+  id = 1:n,
+  type = types,
+  Z = rbinom(n, 1, 0.5),
+  Y0 = case_when(
+    type == "complier" ~ rnorm(n, mean = 2, sd = 1),
+    type == "always" ~ rnorm(n, mean = 1, sd = 1),
+    type == "never" ~ rnorm(n, mean = 3, sd = 1)
+  ),
+  tau = case_when(
+    type == "complier" ~ 2,
+    type == "always" ~ 1,
+    type == "never" ~ 0
+  )
+) |>
+  mutate(
+    D = case_when(
+      type == "complier" ~ Z,
+      type == "always" ~ 1,
+      type == "never" ~ 0
+    ),
+    Y = Y0 + tau * D
+  )
+
+# Estimates
+naive <- df |> summarize(naive = mean(Y[D == 1]) - mean(Y[D == 0])) |> pull()
+
+ITT <- df |> summarize(ITT = mean(Y[Z == 1]) - mean(Y[Z == 0])) |> pull()
+
+PrC <- df |> summarize(fs = mean(D[Z == 1]) - mean(D[Z == 0])) |> pull()
+
+LATE <- ITT / first_stage
+
+ATE <- mean(df$tau)
+
+# Show all in a tibble
+results <- tibble(
+  Estimate = c("Naive", "ITT", "LATE", "ATE"),
+  Value = c(naive, ITT, LATE, ATE)
+)
+
+print(results)
+
+lm(Y~D,data=df) %>% summary()
+lm(Y~Z,data=df) %>% summary()
+lm(D~Z,data=df) %>% summary()
+ivreg(Y~ D | Z, data=df) %>% summary()
